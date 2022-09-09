e24a1c15365b5af8d8bf334f6a39a33d7f9a3a21
Lixin Chen <lixinchen@quickenloans.com>
2022-09-09 19:53:41 -0400

    init checkin

---
+++ TextParser/Parser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextParser
+{
+    public class Parser
+    {
+        public char Delimiter { get; set; }
+        public bool SetDelimiter(string line)
+        {
+            try
+            {
+                if (line.Contains('|'))
+                {
+                    this.Delimiter = '|';
+                }
+                else if (line.Contains(','))
+                {
+                    this.Delimiter = ',';
+                }
+                else
+                {
+                    this.Delimiter = ' ';
+                }
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
