751e6a541945ae5046e4a83149096fda94efee8c
Lixin Chen <lixinchen@quickenloans.com>
2022-09-10 15:05:46 -0400

    Add API project and API test project

---
+++ EmployeeServiceAPI/Services/EmployeeDataService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeDataParser;
+
+namespace EmployeeServiceAPI.Services
+{
+    public class EmployeeDataService
+    {
+        private List<Employee> Employees { get; set; }
+
+        public EmployeeDataService()
+        {
+            Employees = new List<Employee>();
+        }
+
+        public bool AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+            return true;
+        }
+
+        public Employee GetEmployeeByLastName(string name)
+        {
+            return Employees.Where(e => e.LastName == name).FirstOrDefault();
+        }
+        public List<Employee> GetEmployeeByFavriteColor(string color)
+        {
+            return Employees.Where(e => e.FavoriteColor == color).ToList();
+        }
+    }
+}
